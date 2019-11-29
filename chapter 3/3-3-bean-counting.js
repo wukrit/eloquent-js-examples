45cfccae5936de0884839f435b29ab9e4c77d486
Wukrit <sukritwalia@gmail.com>
2019-11-29 17:26:56 -0600

    modified countChar function to use shallow equal

--- chapter 3/3-3-bean-counting.js
+++ chapter 3/3-3-bean-counting.js
@@ -3,7 +3,7 @@ let countBs = (str) => countChar(str, "B")
 function countChar(str, char) {
   let count = 0
   for (let i = 0; i < str.length; i++) {
-    if (str[i] === char) count++;
+    if (str[i] == char) count++;
   }
   return count
 }
