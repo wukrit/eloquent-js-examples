ec2212fd248939da498647a3ae221b25c7a7601d
Wukrit <sukritwalia@gmail.com>
2019-11-29 17:16:42 -0600

    ex 3-2 done

---
+++ 3-2-recursion.js
@@ -0,0 +1,14 @@
+// Your code here.
+function isEven(num) {
+  if (num == 1) return false;
+  if (num == 0) return true;
+  if (num < 0) return isEven(-num);
+  return isEven(num - 2);
+}
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → false
